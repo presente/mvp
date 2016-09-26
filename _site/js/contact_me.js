a67d8845c022a2a05e52f5ffabbaf9bd6b9058cc
Erick <champala86@gmail.com>
2016-09-26 11:42:43 -0700

    plegde form

--- _site/js/contact_me.js
+++ _site/js/contact_me.js
@@ -1,5 +1,4 @@
 $(function() {
-
     $("input,textarea").jqBootstrapValidation({
         preventSubmit: true,
         submitError: function($form, event, errors) {
@@ -10,22 +9,22 @@ $(function() {
             // get values from FORM
             var name = $("input#name").val();
             var email = $("input#email").val();
-            var phone = $("input#phone").val();
-            var message = $("textarea#message").val();
+            var zip = $("input#zip").val();
+            // var message = $("textarea#message").val();
             var firstName = name; // For Success/Failure Message
             // Check for white space in name for Success/Fail message
             if (firstName.indexOf(' ') >= 0) {
                 firstName = name.split(' ').slice(0, -1).join(' ');
             }
             $.ajax({
-                url: "././mail/contact_me.php",
-                type: "POST",
-                data: {
-                    name: name,
-                    phone: phone,
-                    email: email,
-                    message: message
-                },
+                // url: "././mail/contact_me.php",
+                // type: "POST",
+                // data: {
+                //     name: name,
+                //     phone: phone,
+                //     email: email,
+                //     message: message
+                // },
                 cache: false,
                 success: function() {
                     // Success message
@@ -33,7 +32,7 @@ $(function() {
                     $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
                         .append("</button>");
                     $('#success > .alert-success')
-                        .append("<strong>Your message has been sent. </strong>");
+                        .append("<strong>Thank you!</strong>");
                     $('#success > .alert-success')
                         .append('</div>');
 
