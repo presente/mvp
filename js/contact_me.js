0f1cacf9c4e26dd20afe1f1df565690da0e2d105
Erick <champala86@gmail.com>
2016-09-27 00:34:06 -0700

    shareprogress

--- js/contact_me.js
+++ js/contact_me.js
@@ -17,14 +17,6 @@ $(function() {
                 firstName = name.split(' ').slice(0, -1).join(' ');
             }
             $.ajax({
-                // url: "././mail/contact_me.php",
-                // type: "POST",
-                // data: {
-                //     name: name,
-                //     phone: phone,
-                //     email: email,
-                //     message: message
-                // },
                 cache: false,
                 success: function() {
                     // Success message
@@ -32,7 +24,7 @@ $(function() {
                     $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
                         .append("</button>");
                     $('#success > .alert-success')
-                        .append("<strong>Thank you!</strong>");
+                        .append("<strong><h4>Thank you!</h4></strong>");
                     $('#success > .alert-success')
                         .append('</div>');
 
